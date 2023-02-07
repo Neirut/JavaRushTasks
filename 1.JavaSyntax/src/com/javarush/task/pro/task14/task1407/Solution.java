d642b4eb7e684a94f5649b8d6a3f16196ec41932
Oleg <tarasov_oleg@hotmail.com>
2023-02-08 01:14:20 +0200

    08.02.2023

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1407/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task14.task1407;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Lion lion = new Lion();
+        lion.eat(new Food("мясо"));
+        lion.eat(null);
+
+    }
+}
