78f8b3e13d7bd09de36ac779bba9ed5f0ffe6edb
Oleg <tarasov_oleg@hotmail.com>
2023-02-08 18:29:28 +0200

    08.02.2023 2

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1414/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.pro.task14.task1414;
+
+/* 
+Готовим коктейли
+*/
+
+public class Solution {
+
+    public static final String OUTPUT_FORMAT = "Метод %s вызван из строки %d класса %s в файле %s.\n";
+
+    public static void main(String[] args) {
+        makeScrewdriver();
+    }
+
+    public static void printStackTrace(StackTraceElement[] stackTrace) {
+        for (StackTraceElement element: stackTrace) {
+            String nameOfMethod = element.getMethodName();
+            int lineNumber = element.getLineNumber();
+            String className = element.getClassName();
+            String fileName = element.getFileName();
+            System.out.printf(OUTPUT_FORMAT, nameOfMethod, lineNumber, className, fileName);
+
+        }
+    }
+
+    static void makeScrewdriver() {
+        addJuice();
+    }
+
+    static void addJuice() {
+        addVodka();
+    }
+
+    static void addVodka() {
+        printStackTrace(Thread.currentThread().getStackTrace());
+    }
+}
